329d3c20451a640abe87e6cb886763e9f8e4bc0c
Joel Ramalho Filho <joelramalho.negociosonline@gmail.com>
2026-06-29 17:55:41 -0300

    :sparkles: Ex 01 resolvido

--- sistema_de_usuarios_01/src/sistema_de_usuarios_01/Administrador.java
+++ sistema_de_usuarios_01/src/sistema_de_usuarios_01/Administrador.java
@@ -1,5 +1,28 @@
 package sistema_de_usuarios_01;
 
-public class Administrador {
+public class Administrador extends Usuario {
+		
+		private String nivelAcesso;
 
-}
+		public Administrador(String nome, String email, String nivelAcesso) {
+			super(nome, email);
+			this.nivelAcesso = nivelAcesso;
+		}
+
+		public String getNivelAcesso() {
+			return nivelAcesso;
+		}
+
+		public void setNivelAcesso(String nivelAcesso) {
+			this.nivelAcesso = nivelAcesso;
+		}
+		
+		 @Override
+		    public void visualizar() {
+		        System.out.println("Nome: " + getNome() +
+		                           " | Email: " + getEmail() +
+		                           " | Nível de Acesso: " + this.nivelAcesso);
+		    }
+	}
+
+
--- sistema_de_usuarios_01/src/sistema_de_usuarios_01/Usuario.java
+++ sistema_de_usuarios_01/src/sistema_de_usuarios_01/Usuario.java
@@ -1,8 +1,43 @@
 package sistema_de_usuarios_01;
 
 public class Usuario {
-	 String nome;
-	 String email;
-	
+	private String nome;
+	private String email;
+
+	public Usuario(String nome, String email) {
+
+		Validacao.validarNome(nome);
+		Validacao.validarEmail(email);
+
+		this.nome = nome;
+		this.email = email;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		Validacao.validarNome(nome);
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		Validacao.validarEmail(email);
+		this.email = email;
+	}
+
+	public String descricao() {
+		return "Nome: " + this.nome + " | Email: " + this.email;
+
+	}
+
+	public void visualizar() {
+		System.out.println(descricao());
+	}
 
 }
